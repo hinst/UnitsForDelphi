c4cd60b9ef085a240f7d9fe9d5a7464e41139dbf
Alexander Savinoff <hinstance@yandex.ru>
2012-10-17 02:15:52 +0400

    TWindowsOverlappedEvent moved to separate unit

--- UWindowsOverlappedEvent.pas
+++ UWindowsOverlappedEvent.pas
@@ -2,6 +2,114 @@ unit UWindowsOverlappedEvent;
 
 interface
 
+uses
+  Windows,
+  SysUtils,
+  Types;
+
+type
+  EWaitWindowsOverlappedEvent = class(Exception)
+  public
+    constructor Create(const aTime: DWORD);
+  protected
+    fTime: DWORD;
+    procedure CreateThis;
+  public
+    property Time: DWORD read fTime;
+  end;
+
+  TWindowsOverlappedEvent = class
+  public
+    constructor Create;
+  public type
+    EAbandoned = class(EWaitWindowsOverlappedEvent);
+    ETimeOut = class(EWaitWindowsOverlappedEvent);
+    EFailed = class(EWaitWindowsOverlappedEvent);
+  private
+    fOverlapped: TOverlapped;
+    function AccessOverlapped: POverlapped;
+    function GetEvent: THandle;
+    procedure CreateThis;
+    procedure CloseEvent;
+    procedure DestroyThis;
+  public
+    property Event: THandle read GetEvent;
+    property Overlapped: POverlapped read AccessOverlapped;
+    function Wait(const aTime: DWORD): boolean;
+    destructor Destroy; override;
+  end;
+
+
 implementation
+
+constructor EWaitWindowsOverlappedEvent.Create(const aTime: DWORD);
+begin
+  inherited Create('');
+  fTime := aTime;
+  CreateThis;
+end;
+
+procedure EWaitWindowsOverlappedEvent.CreateThis;
+begin
+  Message := 'Time: ' + IntToStr(Time);
+end;
+
+constructor TWindowsOverlappedEvent.Create;
+begin
+  inherited Create;
+  CreateThis;
+end;
+
+function TWindowsOverlappedEvent.AccessOverlapped: POverlapped;
+begin
+  result := @fOverlapped;
+end;
+
+procedure TWindowsOverlappedEvent.CreateThis;
+begin
+  fOverlapped.hEvent := CreateEvent(nil, false, false, nil);
+end;
+
+procedure TWindowsOverlappedEvent.CloseEvent;
+begin
+  if Event > 0 then
+  begin
+    CloseHandle(Event);
+    fOverlapped.hEvent := 0;
+  end;
+end;
+
+function TWindowsOverlappedEvent.GetEvent: THandle;
+begin
+  result := fOverlapped.hEvent;
+end;
+
+procedure TWindowsOverlappedEvent.DestroyThis;
+begin
+  CloseEvent;
+end;
+
+function TWindowsOverlappedEvent.Wait(const aTime: DWORD): boolean;
+var
+  waitResult: DWORD;
+begin
+  result := false;
+  Assert(Event > 0);
+  waitResult := WaitForSingleObject(Event, aTime);
+  if waitResult = WAIT_ABANDONED then
+    raise EAbandoned.Create(aTime);
+  if waitResult = WAIT_FAILED then
+    raise EFailed.Create(aTime);
+  result := waitResult = WAIT_OBJECT_0;
+end;
+
+destructor TWindowsOverlappedEvent.Destroy;
+begin
+  CloseHandle(Event);
+  Overlapped^.hEvent := 0;
+  DestroyThis;
+  inherited Destroy;
+end;
+
 
 end.
--- WindowsPipes.pas
+++ WindowsPipes.pas
@@ -33,37 +33,6 @@ type
     destructor Destroy; override;
   end;
 
-  EWaitWindowsOverlappedEvent = class(Exception)
-  public
-    constructor Create(const aTime: DWORD);
-  protected
-    fTime: DWORD;
-    procedure CreateThis;
-  public
-    property Time: DWORD read fTime;
-  end;
-
-  TWindowsOverlappedEvent = class
-  public
-    constructor Create;
-  public type
-    EAbandoned = class(EWaitWindowsOverlappedEvent);
-    ETimeOut = class(EWaitWindowsOverlappedEvent);
-    EFailed = class(EWaitWindowsOverlappedEvent);
-  private
-    fOverlapped: TOverlapped;
-    function AccessOverlapped: POverlapped;
-    function GetEvent: THandle;
-    procedure CreateThis;
-    procedure CloseEvent;
-    procedure DestroyThis;
-  public
-    property Event: THandle read GetEvent;
-    property Overlapped: POverlapped read AccessOverlapped;
-    function Wait(const aTime: DWORD): boolean;
-    destructor Destroy; override;
-  end;
-
 implementation
 
 constructor TCommonPipe.Create(const aName: string);
@@ -91,72 +60,5 @@ begin
   inherited Destroy;
 end;
 
-constructor EWaitWindowsOverlappedEvent.Create(const aTime: DWORD);
-begin
-  inherited Create('');
-  fTime := aTime;
-end;
-
-procedure EWaitWindowsOverlappedEvent.CreateThis;
-begin
-  Message := 'Time: ' + IntToStr(Time);
-end;
-
-constructor TWindowsOverlappedEvent.Create;
-begin
-  inherited Create;
-  CreateThis;
-end;
-
-function TWindowsOverlappedEvent.AccessOverlapped: POverlapped;
-begin
-  result := @fOverlapped;
-end;
-
-procedure TWindowsOverlappedEvent.CreateThis;
-begin
-  fOverlapped.hEvent := CreateEvent(nil, false, false, nil);
-end;
-
-procedure TWindowsOverlappedEvent.CloseEvent;
-begin
-  if Event > 0 then
-  begin
-    CloseHandle(Event);
-    Overlapped^.hEvent := 0;
-  end;
-end;
-
-procedure TWindowsOverlappedEvent.DestroyThis;
-begin
-  CloseEvent;
-end;
-
-function TWindowsOverlappedEvent.GetEvent: THandle;
-begin
-  result := fOverlapped.hEvent;
-end;
-
-function TWindowsOverlappedEvent.Wait(const aTime: DWORD): boolean;
-var
-  waitResult: DWORD;
-begin
-  result := false;
-  Assert(Event > 0);
-  waitResult := WaitForSingleObject(Event, aTime);
-  if waitResult = WAIT_ABANDONED then
-    raise EAbandoned.Create(aTime);
-  if waitResult = WAIT_FAILED then
-    raise EFailed.Create(aTime);
-  result := not (waitResult = WAIT_TIMEOUT);
-end;
-
-destructor TWindowsOverlappedEvent.Destroy;
-begin
-  CloseHandle(Event);
-  Overlapped^.hEvent := 0;
-  DestroyThis;
-  inherited Destroy;
-end;
 
 end.
